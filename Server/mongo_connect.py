10625bb28b75b2d63b843c660e2079a7313081e8
Gabriel Birman <gabrielbirman1@gmail.com>
2018-11-30 01:37:12 -0500

    getusers functionality added from server end

--- Server/mongo_connect.py
+++ Server/mongo_connect.py
@@ -1,9 +1,21 @@
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+from pymongo import MongoClient 
+from urllib.parse import quote_plus
+from pymongo.errors import ConnectionFailure
+import sys 
+from bson import ObjectId
+
+class MyJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super(MyJSONEncoder, self).default(obj)
 
 app = Flask(__name__)
-
+app.json_encoder = MyJSONEncoder
 app.config['MONGO_DBNAME'] = 'tiger_eats_db'
 app.config['MONGO_URI'] = 'mongodb://pfrazao:y7gnykTXHj8j7EK@ds053380.mlab.com:53380/tiger_eats_db'
 
@@ -33,6 +45,15 @@ def findUsers():
         result.append({'name': user['name']})
 
     return jsonify(result)
+
+@app.route('/api/getUsers', methods=['GET'])
+def get_users():
+
+    cursor = mongo.db.users.find()
+    users = []
+    for user in cursor:
+        users.append(user)
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
