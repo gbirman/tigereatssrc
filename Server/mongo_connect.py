d83d42484266d256534707d2706f23977cd36315
Ishan Sinha <isinha@nat-oitwireless-inside-vapornet100-c-11298.Princeton.EDU>
2018-12-02 16:45:17 -0500

    wrappers

--- Server/mongo_connect.py
+++ Server/mongo_connect.py
@@ -59,18 +59,68 @@ def get_users():
         users.append(user)
     return jsonify(users)
 
-
-@app.route('/api/get_user_email', methods=['GET'])
-def get_user_email(id: str):
-    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
-    return user['email']
+def _get_user(id: str)
+    return mongo.db.users.find({"_id": ObjectId(id)})[0]
 
 
 @app.route('/api/get_user_email', methods=['GET'])
 def get_user_email(id: str):
-    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    user = _get_user(id)
     return user['email']
 
+
+@app.route('/api/get_user_firstname', methods=['GET'])
+def get_user_firstname(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['firstname']
+
+
+@app.route('/api/get_user_lastname', methods=['GET'])
+def get_user_lastname(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['lastname']
+
+
+@app.route('/api/get_user_gender', methods=['GET'])
+def get_user_gender(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['gender']
+
+
+@app.route('/api/get_user_height', methods=['GET'])
+def get_user_lastname(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['height']
+
+
+@app.route('/api/get_user_weight', methods=['GET'])
+def get_user_weight(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['weight']
+
+
+@app.route('/api/get_user_restrictions', methods=['GET'])
+def get_user_restrictions(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['restrictions']
+
+
+@app.route('/api/get_user_calorie_goal', methods=['GET'])
+def get_user_calorie_goal(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['calorie_goal']
+
+
+@app.route('/api/get_user_protein_goal', methods=['GET'])
+def get_user_protein_goal(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['protein_goal']
+
+
+@app.route('/api/get_user_fats_goal', methods=['GET'])
+def get_user_fats_goal(id: str):
+    user = mongo.db.users.find({"_id": ObjectId(id)})[0]
+    return user['fats_goal']
 
 if __name__ == '__main__':
     print(get_user_email("5bf8ca12e7179a56e21592c5"))
