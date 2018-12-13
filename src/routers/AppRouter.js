bcd8b959ae9924ce0c232c1de8b3d4fdc77001ab
Gabriel Birman <gabrielbirman1@gmail.com>
2018-12-13 17:29:20 -0500

    merged with Paolo's stuff

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,7 @@ import DashboardPage from '../components/DashboardPage';
 import NavHeader from '../components/NavHeader';
 import StudentGoalsPage from '../components/StudentGoalsPage';
 import ChangeGoalsPage from '../components/ChangeGoalsPage';
+import ProgressPage from '../components/ProgressPage';
 import TestPage from '../components/TestPage';
 import LoginPage from '../components/LoginPage';
 
@@ -32,6 +33,7 @@ export default class AppRouter extends React.Component {
                         <Route path="/test" component={StudentGoalsPage} exact={true} />
                         <Route path="/changeGoals/:id/:fullname/:calorie_goal/:protein_goal/:fats_goal/:carbs_goal" component={ChangeGoalsPage} />
                         <Route path={"/verified/true"} component={DashboardPage} exact/>
+                        <Route path="/progress" component={ProgressPage} />
                     </Switch>
                 </div>
             </BrowserRouter>
