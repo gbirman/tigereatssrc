2e9cbc2e93cf1632372d693c205539046d651fb5
Ishan Sinha <isinha@Ishans-MacBook-Pro.local>
2019-01-01 21:48:45 -0800

    trying out cas stuff on new setup

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -73,7 +73,7 @@ export default withStyles(styles)(class LoginPage extends React.Component {
                         <div><h1>Welcome to <span style={{padding: 0, margin: 0, color: "#d9f495"}}>Tiger</span>Eats!</h1></div>
                     </Paper>
                     <Grid item xs={3} >
-                        <NavLink to="/dash" justify="center"  style={{ textDecoration: 'none'}}><Button className={classes.loginButton} variant="contained" color="primary" onClick={() => {this.props.onLogin(); alert("SHECK WES GANG GANG!")}}>Login with CAS</Button></NavLink>
+                        <Button className={classes.loginButton} variant="contained" color="primary" href='http://localhost:5000/cas'>Login with CAS</Button>
                     </Grid>
                 </Grid>
             </div>
