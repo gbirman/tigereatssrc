72f5d58351065d88f5a7858bcaa36f9ec7633174
Ishan Sinha <isinha@Ishans-MacBook-Pro.local>
2019-01-04 19:28:01 -0800

    removed the unnecessary stuff that I had added again

--- Server/mongo_connect.py
+++ Server/mongo_connect.py
@@ -65,13 +65,6 @@ app.secret_key = secret_key
 app.wsgi_app = SessionMiddleware(app.wsgi_app, session_opts)
 app.session_interface = BeakerSessionInterface()
 
-mongo = PyMongo(app)
-CORS(app)
-
-
-# FUUUUUUCKKKKK
-
-
 # casClient = CASClient()  # in order to make the decorator work
 
 @app.route('/get_netid', methods=['GET'])
