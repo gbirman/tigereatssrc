f6e3dfe3c50dcc77d9f72f8e1e3e7a0892f51c36
Ishan Sinha <isinha@nat-oitwireless-inside-vapornet100-d-12602.Princeton.EDU>
2019-01-13 20:16:20 -0500

    trying to make it cleaner

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -43,7 +43,7 @@ export default withStyles(styles)(class NavHeader extends React.Component {
                             <NavLink to="/dash" style={{ textDecoration: 'none'}}><Button className={classes.headerButton} variant="contained" color="primary">Dashboard</Button></NavLink>
                         </Grid>
                         <Grid item>
-                            <Button className={classes.headerButton} variant="contained" color="primary" href="https://fed.princeton.edu/cas/logout">Logout</Button>
+                            <NavLink to="/" style={{ textDecoration: 'none'}}><Button className={classes.headerButton} variant="contained" color="primary" href="https://fed.princeton.edu/cas/logout">Logout</Button></NavLink>
                         </Grid>
                     </Grid>
                 </Grid>
