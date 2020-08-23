097ad7cbc74cc30618a4db42c0ead2a9a3c26871
Gabriel <25272206+gbirman@users.noreply.github.com>
2020-08-23 13:12:46 -0400

    test

---
+++ build/precache-manifest.5e93ef1cbe2af51453c033c2758220af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "89652a94e0e14103d05d",
+    "url": "/static/css/main.f0ef3ffe.chunk.css"
+  },
+  {
+    "revision": "89652a94e0e14103d05d",
+    "url": "/static/js/main.89652a94.chunk.js"
+  },
+  {
+    "revision": "35bbba6b3543c872a7ff",
+    "url": "/static/js/1.35bbba6b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f53021612816239ae305208e0af9f44",
+    "url": "/static/media/Jamie.3f530216.jpg"
+  },
+  {
+    "revision": "fe7ef0abb5737e6f5cd3dda749f342d4",
+    "url": "/static/media/Gabe.fe7ef0ab.jpg"
+  },
+  {
+    "revision": "105b8c62eabc85a0477025d9620034c2",
+    "url": "/static/media/Ishan.105b8c62.jpg"
+  },
+  {
+    "revision": "9dbb91154dbed7c6d514c47a324eca30",
+    "url": "/static/media/Paulo.9dbb9115.jpg"
+  },
+  {
+    "revision": "c0be1774ddc9f914bcdf2378f068b6a2",
+    "url": "/static/media/PersonPlaceholder.c0be1774.jpg"
+  },
+  {
+    "revision": "64ba59543daad2415a12e39e4e7e6179",
+    "url": "/static/media/EggToast.64ba5954.jpg"
+  },
+  {
+    "revision": "81ff3e20dafbca3f1c053851e4f2bfd7",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.3cea0f9ec3286a763a291ed0a38298e8.js"
+  "/precache-manifest.5e93ef1cbe2af51453c033c2758220af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/build/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
